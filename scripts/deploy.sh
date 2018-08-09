aae0151ae5811a17d6039ba37f75cba508f28e34
ashwinath <ashwinath@hotmail.com>
2018-08-09 16:45:14 +0800

    fix: travis docker key

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,1 +1,3 @@
-ssh -o "StrictHostKeyChecking no" travis@ashwinchat.com -t "docker pull ashwinath/poe-search-bot && docker stop poe-search-bot && docker rm poe-search-bot && docker run --name poe-search-bot -e DISCORD_TOKEN=`${DISCORD_TOKEN}` -d ashwinath/poe-search-bot && docker image prune -f"
+#!/bin/bash
+
+ssh -o "StrictHostKeyChecking no" travis@ashwinchat.com -t "curl -XDELETE 'localhost:9200/items' && docker pull ashwinath/poe-search-bot && docker stop poe-search-bot && docker rm poe-search-bot && docker run --name poe-search-bot -e ES_HOST='10.15.0.5:9200' -e DISCORD_TOKEN=${DISCORD_TOKEN} -d ashwinath/poe-search-bot && docker image prune -f"
