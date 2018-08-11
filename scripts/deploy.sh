57b9e460c534074bd0890d84337f047ce06179b0
ashwinath <ashwinath@hotmail.com>
2018-08-11 12:15:26 +0800

    expose port

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ssh -o "StrictHostKeyChecking no" travis@ashwinchat.com -t "curl -XDELETE 'localhost:9200/items' && docker pull ashwinath/poe-search-bot && docker stop poe-search-bot && docker rm poe-search-bot && docker run --name poe-search-bot -e ES_HOST='10.15.0.5:9200' -e DISCORD_TOKEN=${DISCORD_TOKEN} -d ashwinath/poe-search-bot && docker image prune -f"
+ssh -o "StrictHostKeyChecking no" travis@ashwinchat.com -t "curl -XDELETE 'localhost:9200/items' && docker pull ashwinath/poe-search-bot && docker stop poe-search-bot && docker rm poe-search-bot && docker run --name poe-search-bot -e ES_HOST='10.15.0.5:9200' -e DISCORD_TOKEN=${DISCORD_TOKEN} -p 7000:7000 -d ashwinath/poe-search-bot && docker image prune -f"
